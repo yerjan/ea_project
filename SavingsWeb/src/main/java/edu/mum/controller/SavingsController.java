28e775b77d8e2d566ebfe12e2f5357c6e2c7dc43
ggganzo <ggganzo@gmail.com>
2017-07-18 17:31:31 -0500

    changes on Savings.jsp and related Classes

---
+++ SavingsWeb/src/main/java/edu/mum/controller/SavingsController.java
@@ -0,0 +1,51 @@
+package edu.mum.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import edu.mum.domain.Customer;
+import edu.mum.domain.Savings;
+import edu.mum.service.CustomerService;
+import edu.mum.service.SavingsService;
+
+@Controller
+@RequestMapping({"/customers"})
+public class SavingsController {
+	
+	@Autowired
+	private CustomerService  customerService;
+
+	@Autowired
+	private SavingsService savingsService;
+
+	@RequestMapping({"", "/all"})
+	public String listUsers(Model model) {
+		System.out.println("heree!");
+		List<Customer> customers = customerService.findAll();
+		model.addAttribute("customers", customers);
+		
+		return "customers";
+	}
+	
+  	@RequestMapping("/{id}")
+	public String getUserById(@PathVariable("id") Long id,Model model) {
+		Customer customer = customerService.findOne(id);
+		List<Savings> savings = savingsService.findByCustomerId(customer.getId());
+		
+		model.addAttribute("customer", customer);
+		model.addAttribute("savings", savings);
+
+ 		return "customer";
+	}
+ 
+}
