ecbdff42f4314807e1d74327a3b933f62bcdd37b
Frehiwot <frefrehowot05@gmail.com>
2017-07-19 10:32:07 -0600

    Aspect with logging is on the way

--- SavingsRest/src/main/java/edu/mum/service/SavingsService.java
+++ SavingsRest/src/main/java/edu/mum/service/SavingsService.java
@@ -7,7 +7,7 @@ import edu.mum.domain.Savings;
 import edu.mum.domain.Transaction;
 
 public interface SavingsService {
-
+	//Aop
 	public void save(Savings savings);
 
 	public List<Savings> findAll();
@@ -15,15 +15,15 @@ public interface SavingsService {
 	public List<Savings> findByCustomer(Long customerId);
 
 	public Savings findOne(Long id);
-
+	//Aop
 	public Savings update(Savings user);
-
+	//Aop
 	public Savings incrementBalance(Transaction tran);
-
+	//Aop
 	public Savings decrementBalance(Transaction tran);
-
+	//Aop
 	public Savings openSavings(Long id);
-
+//Aop
 	public Savings closeSavings(Long id);
 
 	public List<Transaction> listTransaction(Long id);
