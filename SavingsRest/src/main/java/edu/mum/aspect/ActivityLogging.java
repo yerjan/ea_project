3f5f9a1320b62b675447fc636f3292cff48d951e
erka <erjan.mg@gmail.com>
2017-07-21 20:29:10 -0500

    aspect bolhgui biana

--- SavingsRest/src/main/java/edu/mum/aspect/ActivityLogging.java
+++ SavingsRest/src/main/java/edu/mum/aspect/ActivityLogging.java
@@ -1,23 +1,11 @@
 package edu.mum.aspect;
 
-import java.util.Date;
-import java.util.logging.Logger;
-
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import edu.mum.dao.ActivityDao;
-import edu.mum.dao.impl.ActivityDaoImp;
-import edu.mum.domain.Activity;
-import edu.mum.domain.Customer;
-import edu.mum.domain.Savings;
-import edu.mum.domain.Staff;
-import edu.mum.domain.StaffCredentials;
 import edu.mum.domain.Transaction;
 import edu.mum.service.ActivityService;
 
@@ -27,26 +15,35 @@ public class ActivityLogging{
 	@Autowired
 	private ActivityService activityService;
 	
-	@Pointcut("execution(* edu.mum.service.SavingsService.createSavings)")
-	public void createSavings(){}
+//	@Pointcut("execution(* edu.mum.service.SavingsService.createSavings(..))")
+//	public void createSavings(){}
+//	
+//	@Pointcut("execution(* edu.mum.service.SavingsService.incrementBalance(..))")
+//	public void incrementBalance(){}
 	
-	@Pointcut("execution(* edu.mum.service.SavingsService.incrementBalance(transaction))")
-	public void incrementBalance(Transaction transaction){}
+//	@Pointcut("within(edu.mum.serviceimpl..*)")
+//	public void decrementBalance(){}
 	
-	@Pointcut("execution(* edu.mum.service.SavingsService.decrementBalance(transaction))")
-	public void decrementBalance(Transaction transaction){}
+	@Pointcut("execution(* edu.mum.serviceimpl.SavingsServiceImpl.decrementBalance(..))")
+	public void decrement(){}
 	
-	
-
-	@Before("incrementBalance(transaction) && decrementBalance(transaction)")
-	public void save(Transaction transaction){
-		Activity activity = new Activity();
-		activity.setUser((long) 1);
-		activity.setEvent("Withdraw balance");
-		activity.setData("Type: " + transaction.getType() + ", Amount: " + transaction.getAmount());
-		
-		activityService.save(activity);
+	@Before("decrement()")
+	public void something(){
+		System.out.println(">>>>>>>>>>>>>IMPL BOLJ BAINA!        !!!!!!!!!!!!!!!!!!!!");
 	}
+//
+//	@Before("incrementBalance()")
+//	public void save(Transaction transaction){
+//		System.out.println(">>>>>>>>>>>>>>>>>>>>> ASPECT! >>>>>>>>>>>>>>>>>>>>>>>");
+////		Activity activity = new Activity();
+////		activity.setUser((long) 1);
+////		activity.setEvent("Withdraw balance");
+//		//activity.setData("Type: " + transaction.getType() + ", Amount: " + transaction.getAmount());
+//		
+//		//System.out.println("Type: " + transaction.getType() + ", Amount: " + transaction.getAmount());
+//	
+//		//activityService.save(activity);
+//	}
 		
 //		@Before()
 //		public void update(Savings user){
